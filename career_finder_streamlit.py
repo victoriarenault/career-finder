50a0a8bb701a573f372693c27459947afeed065c
victoriarenault <victoria.renault@qmc.ac.uk>
2025-01-29 11:24:09 +0000

    Update career_finder_streamlit.py

--- career_finder_streamlit.py
+++ career_finder_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.image("https://raw.githubusercontent.com/victoriarenault/career-finder/main/logo.png", width=200)
+st.image("https://raw.githubusercontent.com/victoriarenault/career-finder/main/Logo.png", width=200)
 # Load career data from Excel file
 file_path = "Draft Master - Career Pathways.xlsx"
 
